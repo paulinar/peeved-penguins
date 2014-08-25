8b5225c72f64f7b999ac6f9b9240018dc54de96f
Paulina <paulinaramos@unknown7cd1c3e8d8f3.gateway.2wire.net>
2014-08-25 13:19:56 -0700

    Add functioning game

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,15 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Benjamin Encz on 16/01/14.
+//  Copyright (c) 2014 MakeGamesWithUs Inc. Free to use for all purposes.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@property (nonatomic, assign) BOOL launched;
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Benjamin Encz on 16/01/14.
+//  Copyright (c) 2014 MakeGamesWithUs Inc. Free to use for all purposes.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
